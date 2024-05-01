2b3bc337c0a25f3bda820eb71470474edf9d712c
Carlos Guillermo Baldracco <cgbaldracco@gmail.com>
2024-05-01 14:41:07 -0300

    Arreglo code smell type test, elimino codigo irrelevante.

--- src/main/java/dds/monedero/model/Movimiento.java
+++ src/main/java/dds/monedero/model/Movimiento.java
@@ -24,24 +24,12 @@ public class Movimiento {
     return fecha;
   }
 
-  //type test, metodos innecesarios que nisiquiera son utilizados
-  public boolean fueDepositado(LocalDate fecha) {
-    return isDeposito() && esDeLaFecha(fecha);
-  }
-  public boolean fueExtraido(LocalDate fecha) {
-    return isExtraccion() && esDeLaFecha(fecha);
-  }
-
   public boolean esDeLaFecha(LocalDate fecha) {
     return this.fecha.equals(fecha);
   }
 
   public boolean isDeposito() {
     return esDeposito;
-  }
-
-  public boolean isExtraccion() {
-    return !esDeposito;
   }
 
   //estos dos los veo como feature envy
